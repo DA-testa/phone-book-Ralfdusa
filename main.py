bcc2a71f0f6d17d4567ba299a49ddeb35004209f
Ralfdusa <125834007+Ralfdusa@users.noreply.github.com>
2023-04-03 01:37:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,16 @@
-# python3
+n = int(input())
+phone_book = {}
 
-class Query:
-    def __init__(self, query):
-        self.type = query[0]
-        self.number = int(query[1])
-        if self.type == 'add':
-            self.name = query[2]
-
-def read_queries():
-    n = int(input())
-    return [Query(input().split()) for i in range(n)]
-
-def write_responses(result):
-    print('\n'.join(result))
-
-def process_queries(queries):
-    result = []
-    # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
-    for cur_query in queries:
-        if cur_query.type == 'add':
-            # if we already have contact with such number,
-            # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
-        elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+for i in range(n):
+    query = input().split()
+    if query[0] == "add":
+        phone_book[query[1]] = query[2]
+    elif query[0] == "del":
+        if query[1] in phone_book:
+            del phone_book[query[1]]
+    elif query[0] == "find":
+        if query[1] in phone_book:
+            print(phone_book[query[1]])
         else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
-    return result
-
-if __name__ == '__main__':
-    write_responses(process_queries(read_queries()))
-
+            print("not found")
+            
